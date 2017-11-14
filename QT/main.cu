9d290022821a402287420ac1bcaf03586fae1e12
swepstonius <ian.swepston@gmail.com>
2017-11-13 21:49:58 -0600

    Now does all stages for single round 64bit

--- QT/main.cu
+++ QT/main.cu
@@ -34,8 +34,6 @@ __device__ byte doShifting(int * shiftArray, byte * input, int id)
         temp = temp >> (8-(bit+1)%8)%8; // shift to position 0
         temp &= 0x01; // mask in only this bit
 
-//        if(get_idx() == 6) printf("%d: %02x\n", i, temp);
-
         temp = temp << 7 - i%8; // shift to right position
 
         // set bits
@@ -102,7 +100,7 @@ __global__ void cuLeftCircShifts(byte * input, byte * result)
         result[i] = (out >> (6 - i%7)*8) & 0x0FF;
     }
 
-    printf("id %d: %lx\n", id, out);
+    //    printf("id %d: %lx\n", id, out);
 }
 
 __global__ void cuPC2(byte * input, byte * round_keys)
@@ -110,14 +108,106 @@ __global__ void cuPC2(byte * input, byte * round_keys)
     int id = get_idx();
     int round = (id/6);
 
-    byte * x = &input[round*7];
-    for(int i = 0; i < 7; i++) {
-        if(id==7) printf("%02X\n", x[i]);
-    }
+    //    byte * x = &input[round*7];
+    //    for(int i = 0; i < 7; i++) {
+    //        if(id==7) printf("%02X\n", x[i]);
+    //    }
 
     byte temp = doShifting(PC_2, &input[round*7], id%6);
-    if(id == 7) printf("Res: %0x\n", temp);
+    //    if(id == 7) printf("Res: %0x\n", temp);
     round_keys[id] = temp;
+}
+
+// 64 -> 64
+__global__ void cuIP(byte * input, byte * output)
+{
+    int id = get_idx();
+
+    output[id] = doShifting(IP, input, id);
+
+    printf("IP: id = %d: %02x\n", id, output[id]);
+}
+
+// 32(64) -> 48
+__global__ void cuEPerm(byte * input, byte * output)
+{
+    int id = get_idx();
+
+    output[id] = doShifting(Ex, input, id%3);
+
+    printf("Ex: id = %d: %02x\n", id, output[id]);
+}
+
+// 48 -> 48
+__global__ void cuXOR(byte * input, byte * key, byte * output)
+{
+    int id = get_idx();
+
+    output[id] = input[id] ^ key[id%6];
+
+    printf("XOR: id = %d: %02x\n", id, output[id]);
+}
+
+// 48 -> 32
+__global__ void cuSBoxes(byte * input, byte * output)
+{
+    int id = get_idx();
+    output[id] = 0;
+
+    uint64_t value = 0;
+    for(int i = 0; i < 6; i++) {
+        value |= input[i];
+        value = value << 8;
+    }
+
+    value = value >> 8;
+
+    int modId = id % 8;
+    byte test = (value >> (7 - modId)*6) & 0x3F;
+    byte row = ((test >> 4) & 0x02) | (test & 0x01); // outer bits
+    byte col = (test >> 1) & 0x0F; // inner bits
+
+//    if (id == 0) printf("%lx\n", value);
+
+    int offset = row * 16 + col;
+    byte result = boxes[id%8][offset];
+
+    output[id] |= result << 4*(1 - (id % 2));
+    __syncthreads();
+
+    if(id % 2 == 0)
+        output[id] = output[id] | output[id + 1];
+
+    __syncthreads();
+
+    if(id % 8 == 0) {
+        output[id + 1] = output[id + 2];
+        output[id + 2] = output[id + 4];
+        output[id + 3] = output[id + 6];
+
+        printf("S-BOX: %02X%02X%02X%02X\n", output[id + 0], output[id + 1], output[id + 2], output[id + 3]);
+    }
+
+//    printf("SBOX: id = %d: %02X -> shamt %d row: %d, col %d, res: %x, out %02x\n", id, test, (7 - modId)*6, row, col, result, output[id]);
+}
+
+__global__ void cuPPerm(byte * input, byte * output)
+{
+    int id = get_idx();
+
+    output[id] = doShifting(Pf, input, id%4);
+
+    printf("P: id = %d: %02x\n", id, output[id]);
+}
+
+__global__ void cuCombine(byte * L, byte * R, byte * output)
+{
+    int id = get_idx();
+
+    if(id % 8 < 4) output[id] = L[id%4];
+    else output[id] = R[id%4];
+
+    printf("output: id = %d: %02x\n", id, output[id]);
 }
 
 int main(int argc, char **argv)
@@ -134,31 +224,57 @@ int main(int argc, char **argv)
     cudaMalloc((void**)&after_shift, sizeof(byte)*16*7);
     cudaMalloc((void**)&round_keys, sizeof(byte)*16*6);
 
+    byte * holding1;
+    byte * holding2;
+    byte * holding3;
+    cudaMalloc((void**)&holding1, sizeof(byte)*8);
+    cudaMalloc((void**)&holding2, sizeof(byte)*8);
+    cudaMalloc((void**)&holding3, sizeof(byte)*8);
+
+
     byte key[8] = {0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x10};
 
+    byte plaintext[] = {0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x10};
+
     cudaMemcpy(inputKey, &key, sizeof(uint64_t), cudaMemcpyHostToDevice);
+    cudaMemcpy(holding1, &plaintext, sizeof(uint64_t), cudaMemcpyHostToDevice);
 
     // Calculate all keys
     cuPC1<<<1,7>>>(inputKey, output);
     cuLeftCircShifts<<<1,16>>>(output, after_shift);
     cuPC2<<<1,96>>>(after_shift, round_keys); // 96 = 16*6
 
+    cuIP<<<1,8>>>(holding1, holding2);
+    byte * L0 = holding2;
+    byte * R0 = &holding2[4];
+
+    cuEPerm<<<1,6>>>(R0, holding3);
+    cuXOR<<<1,6>>>(holding3, round_keys, holding1);
+
+    cuSBoxes<<<1,8>>>(holding1, holding3);
+    cuPPerm<<<1,4>>>(holding3, holding1);
+
+    cuXOR<<<1,4>>>(holding1, L0, holding3);
+    cuCombine<<<1, 8>>>(R0, holding3, holding1);
+
+
+
     cudaDeviceSynchronize();
 
     byte result[16*6];
     cudaMemcpy(result, round_keys, 16*6, cudaMemcpyDeviceToHost);
 
-    printf("Output: ");
-    for(int i = 0; i < 16; i++) {
-        printf("Key %d:\n", i+1);
+    //    printf("Output: ");
+    //    for(int i = 0; i < 16; i++) {
+    //        printf("Key %d:\n", i+1);
 
-        for(int j = i*6; j < i*6 + 6; j++)
-            printf("%02X",result[j]);
+    //        for(int j = i*6; j < i*6 + 6; j++)
+    //            printf("%02X",result[j]);
 
-        printf("\n");
-    }
+    //        printf("\n");
+    //    }
 
-    printf("\n");
+    //    printf("\n");
 
     return 0;
 }
